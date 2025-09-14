0a33cfe2a1ba360d2edf5f64c48b7b5a2b9daebd
dekwidap <dekwidap28@gmail.com>
2025-09-14 10:40:56 +0800

    update for navigation and stoping music

--- script.js
+++ script.js
@@ -37,3 +37,25 @@ function openDoor() {
         doorSection.css("display", "none");
     }, 2000);
 }
+
+
+// Button Music
+var isPlaying = true
+
+function toogleMusic(event) {
+    event.preventDefault()
+    const musicButton = document.getElementById('music-button')
+    if(isPlaying){
+        musicButton.innerHTML = '<i class="fas fa-fw fa-pause"></i>'
+        musicButton.classList.remove('rotate')
+        musicButton.style.transform = 'translateY(0)'
+        song.pause()
+    }
+    else {
+        musicButton.innerHTML = '<i class="fas fa-fw fa-compact-disc"></i>'
+        musicButton.classList.add('rotate')
+        song.play()
+    }
+
+    isPlaying = !isPlaying
+}
