976ba4540e4214dff2e7eaf83a6a2478abdac5ec
dekwidap <dekwidap28@gmail.com>
2025-10-12 19:32:38 +0800

    removing door function

--- script.js
+++ script.js
@@ -9,39 +9,6 @@ if (tempMusic) {
 }
 song.volume = 0.5; // nilai antara 0.0 - 1.0
 
-// Door Start
-function openDoor() {
-    // Back to Top
-    window.scrollTo(0, 0);
-
-    // Sound Door
-    var soundDoor = document.querySelector(".sound-door");
-    soundDoor.play();
-
-    // Door Section
-    var doorSection = $("#door-section");
-    var doors = document.querySelectorAll(".door");
-    doors.forEach(function (door, index) {
-        var direction = index === 0 ? -1 : 1;
-        door.style.transform = "rotateY(" + 70 * direction + "deg)";
-    });
-
-    // Set Timeout
-    setTimeout(function () {
-        // Music Play
-        song.play();
-        doorSection.css("transform", "scale(6)");
-    }, 600);
-
-    // SetTimeout DoorSection
-    setTimeout(function () {
-        doorSection.css("opacity", 0);
-        $("body").removeClass("overflow-hidden");
-        $("body").addClass("transition");
-        doorSection.css("display", "none");
-    }, 2000);
-}
-
 // Button Music
 var isPlaying = true;
 
